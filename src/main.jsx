0aab0fe296c971f71a3615f9659546058182578a
RobertPaton-Smith <robert.p-s@live.co.uk>
2023-11-20 15:31:36 +0000

    FINAL-TOUCHES: Removed unused css attributes from React default demo

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import App from "./App.jsx"; // app.jsx has some styling that I can't get rid of for now
 
-// const greeting = <h1>Hello React</h1>
-// const greeting = React.createElement("h1", null, "Hello React") // alternative
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App/>
 )
